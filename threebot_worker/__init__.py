7e1426f008ca93503b9e46b514462f202c69e91f
Walter Renner <renner@arteria.ch>
2015-02-19 17:39:48 +0100

    Bump Version 0.0.9

--- threebot_worker/__init__.py
+++ threebot_worker/__init__.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-<<<<<<< HEAD
-__version__ = '0.0.6'
-=======
-__version__ = '0.0.8'
->>>>>>> 77a2ebc967bd005befa8cf21626b9cee04a6b700
+__version__ = '0.0.9'
