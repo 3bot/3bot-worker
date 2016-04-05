d47e524810fa842d32737abe946b205350607b31
Walter Renner <renner@arteria.ch>
2016-04-05 12:34:21 +0200

    make Daemon a New-style class

--- threebot_worker/daemon.py
+++ threebot_worker/daemon.py
@@ -8,7 +8,7 @@ import atexit
 from signal import SIGTERM
 
 
-class Daemon:
+class Daemon(object):
     """
     A generic daemon class.
     Usage: subclass the Daemon class and override the run() method
